bf6845bedfe16ffa58870ed3fbbd5403202e2345
Val Erastov <xibyte@gmail.com>
2022-06-07 05:12:09 +0000

    publish npm package infra

---
+++ scripts/publish-npm.sh
@@ -0,0 +1,20 @@
+
+BASEDIR=$(dirname "$0")
+ROOT=$BASEDIR"/.." 
+PCKG=$ROOT"/npm-package" 
+GIT_SHA=`git rev-parse HEAD`
+
+mkdir $PCKG"/package"
+cp -r $PCKG"/template/." $PCKG"/package"
+cp $ROOT"/build-wasm/main.js" $PCKG"/package/occt.js"
+cp $ROOT"/build-wasm/main.wasm" $PCKG"/package/occt.wasm"
+
+
+
+sed -i -e "s/<SNAPSHOT_SHA>/"$GIT_SHA"/g" $PCKG"/package/package.json"
+
+ls $PCKG"/package/"
+
+cd $PCKG"/package/"
+# npm publish --access public
+cd -
