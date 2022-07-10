14167f0c1c6f3c3e28fbcb50ef322973ebc19def
Val Erastov <xibyte@gmail.com>
2022-07-10 22:35:32 +0000

    Expose SetLocaiton

--- shape-io/main.cpp
+++ shape-io/main.cpp
@@ -2,6 +2,7 @@
 
 #include <emscripten.h>
 #include <DBRep.hxx>
+#include <gp_Trsf.hxx>
 #include "interrogate.hpp"
 #include "historyIO.hpp"
 #include "classify.hpp"
@@ -76,6 +77,49 @@ extern "C" {
   void UpdateTessellation(int shapePtr, double deflection) {
     TopoDS_Shape* shape = reinterpret_cast<TopoDS_Shape*>(shapePtr);
     e0::io::UpdateTessellation(*shape, deflection);
+  }
+
+  EMSCRIPTEN_KEEPALIVE
+  void SetLocation(const char* shapeName, float mx0, float mx1, float mx2, float mx3, float mx4, float mx5, float mx6, float mx7, float mx8, float mx9, float mx10, float mx11) {
+    try {
+
+      std::cout << "Getting " << shapeName << std::endl;
+
+      TopoDS_Shape shape = DBRep::Get(shapeName);
+
+      std::cout << "Got shape " << std::endl;
+      
+      gp_Trsf trfs;
+
+      trfs.SetValues(
+        mx0,
+        mx1,
+        mx2,
+        mx3,
+        mx4,
+        mx5,
+        mx6,
+        mx7,
+        mx8,
+        mx9,
+        mx10,
+        mx11
+      );
+      trfs.SetScaleFactor(1);
+
+      std::cout << "Creating location object " << std::endl;
+      TopLoc_Location loc(trfs);
+
+      std::cout << "Setting location " << std::endl;
+
+      shape.Location(loc);
+
+      std::cout << "Writing shape back " << shapeName << std::endl;
+      DBRep::Set(shapeName, shape);
+
+    } catch (Standard_Failure const& anException) {
+      std::cerr << "ERROR: " << anException.GetMessageString() << std::endl;
+    }
   }
 
 }
